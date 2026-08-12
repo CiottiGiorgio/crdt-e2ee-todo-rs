3086c521c7a36526a40b219187cda624b6102130
CiottiGiorgio <giorgio.ciotti.dev@proton.me>
2026-08-12 23:10:43 +0200

    moving commands in a separate module

---
+++ src-tauri/src/commands.rs
@@ -0,0 +1,41 @@
+use crate::models::{TodoItem, TodoStatus};
+use crate::AppState;
+use tauri::State;
+use tauri_specta::{collect_commands, Builder};
+
+#[tauri::command]
+#[specta::specta]
+pub async fn get_todos(state: State<'_, AppState>) -> Result<Vec<TodoItem>, String> {
+    state.todo_repo.get_all().await
+}
+
+#[tauri::command]
+#[specta::specta]
+pub async fn add_todo(text: String, state: State<'_, AppState>) -> Result<TodoItem, String> {
+    state.todo_repo.add(text).await
+}
+
+#[tauri::command]
+#[specta::specta]
+pub async fn update_todo_status(
+    id: i32,
+    status: TodoStatus,
+    state: State<'_, AppState>,
+) -> Result<(), String> {
+    state.todo_repo.update_status(id, status).await
+}
+
+#[tauri::command]
+#[specta::specta]
+pub async fn delete_todo(id: i32, state: State<'_, AppState>) -> Result<(), String> {
+    state.todo_repo.delete(id).await
+}
+
+pub fn get_specta_builder() -> Builder<tauri::Wry> {
+    Builder::<tauri::Wry>::new().commands(collect_commands![
+        get_todos,
+        add_todo,
+        update_todo_status,
+        delete_todo
+    ])
+}
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,59 +1,21 @@
+mod commands;
 mod constants;
 mod models;
 mod repository;
 
-use models::{TodoItem, TodoStatus};
 use repository::sqlite::SqliteTodoRepo;
 use repository::TodoRepository;
 use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions};
 use std::sync::Arc;
-use tauri::{Manager, State};
-use tauri_specta::{collect_commands, Builder};
+use tauri::Manager;
 
 pub struct AppState {
     pub todo_repo: Arc<dyn TodoRepository>,
 }
 
-#[tauri::command]
-#[specta::specta]
-async fn get_todos(state: State<'_, AppState>) -> Result<Vec<TodoItem>, String> {
-    state.todo_repo.get_all().await
-}
-
-#[tauri::command]
-#[specta::specta]
-async fn add_todo(text: String, state: State<'_, AppState>) -> Result<TodoItem, String> {
-    state.todo_repo.add(text).await
-}
-
-#[tauri::command]
-#[specta::specta]
-async fn update_todo_status(
-    id: i32,
-    status: TodoStatus,
-    state: State<'_, AppState>,
-) -> Result<(), String> {
-    state.todo_repo.update_status(id, status).await
-}
-
-#[tauri::command]
-#[specta::specta]
-async fn delete_todo(id: i32, state: State<'_, AppState>) -> Result<(), String> {
-    state.todo_repo.delete(id).await
-}
-
-fn get_specta_builder() -> Builder<tauri::Wry> {
-    Builder::<tauri::Wry>::new().commands(collect_commands![
-        get_todos,
-        add_todo,
-        update_todo_status,
-        delete_todo
-    ])
-}
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let builder = get_specta_builder();
+    let builder = commands::get_specta_builder();
 
     #[cfg(debug_assertions)]
     builder
